8dfe33ea82117c180a40de8b2eb0bb7d06371cff
Stanislav Vasilyev <creadome@gmail.com>
2013-06-23 23:59:04 +0900

    Изменена структура репозитория

---
+++ gadget/.description.php
@@ -0,0 +1,18 @@
+<?
+	/*
+		BitrixCacheCleaner v1.2 - https://github.com/creadome/BitrixCacheCleaner
+		Быстрая очистка кеша 1С-Битрикс 		 				
+
+		(c) 2013 Станислав Васильев - http://creado.me
+		creadome@gmail.com		
+	*/
+
+	if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+	
+	$arDescription = array(
+		"NAME" => "Очистка кеша",
+		"DESCRIPTION" => "Удаление кеша и миниатюр изображений",
+		"GROUP" => array("ID" => "admin_content"),		
+		"AI" => true	
+	);
+?>
