74958114672912c10dd504320d9124525e150520
Stanislav Vasilyev <creadome@gmail.com>
2013-06-23 15:57:37 +0900

    Добавлен копирайт

--- .description.php
+++ .description.php
@@ -1,5 +1,13 @@
 <?
-	if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+	/*
+		BitrixCacheCleaner v1.2 - https://github.com/creadome/BitrixCacheCleaner
+		Быстрая очистка кеша 1С-Битрикс 		 				
+
+		(c) 2013 Станислав Васильев - http://creado.me
+		creadome@gmail.com		
+	*/
+
+	if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 	
 	$arDescription = array(
 		"NAME" => "Очистка кеша",
